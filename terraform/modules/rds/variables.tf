81168e267a738cdfdddb264a6514bbabb9e46440
robin-vidal <robin.vidal.pro@gmail.com>
2026-06-18 14:41:09 +0200

    fix(terraform): multi-line variable blocks

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -3,4 +3,7 @@ variable "subnet_ids"  { type = list(string) }
 variable "rds_sg_id"   { type = string }
 variable "db_name"     { type = string }
 variable "db_user"     { type = string }
-variable "db_password" { type = string, sensitive = true }
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
